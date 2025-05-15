22f30f734ecad044d009e1eb95faacf2c9fc4327
no609 <133532093+no609@users.noreply.github.com>
2025-05-15 21:47:15 +0530

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -47,17 +47,7 @@ conn.commit()
 
 
 
-os.makedirs('.streamlit', exist_ok=True)
-with open('.streamlit/config.toml', 'w') as f:
-     f.write('''[theme]
-primaryColor = "#08c2af"
-backgroundColor = "#002b36"
-secondaryBackgroundColor = "#586e75"
-textColor = "#ffffff"
 
-[client]
-toolbarMode = "minimal"
-''')
 
 
 
